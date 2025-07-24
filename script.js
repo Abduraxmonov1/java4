94d36f626a36e1c3b4b80f99df2c20518f9c8d22
Abduraxmonov1 <abubakir7083@gmail.com>
2025-07-24 22:49:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let user = {
+    name:'a',
+    password:'b',
+    id:'c',
+};
+
+let a =prompt('write your name')
+
+let b =+prompt('write your password')
+
+let c =+prompt('write your id')
+
+console.log(a,b,c);
+
